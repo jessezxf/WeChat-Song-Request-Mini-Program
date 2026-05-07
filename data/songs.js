879781196349bde72bc671ddaa64f78e5e547761
jesse <94545107+jessezxf@users.noreply.github.com>
2026-05-07 21:24:52 +0800

    歌单

---
+++ data/songs.js
@@ -0,0 +1,74 @@
+const songs = [
+  {
+    id: "s001",
+    name: "晴天",
+    artist: "周杰伦",
+    mood: "青春",
+    tags: ["流行", "合唱", "温柔"]
+  },
+  {
+    id: "s002",
+    name: "起风了",
+    artist: "买辣椒也用券",
+    mood: "晚风",
+    tags: ["流行", "治愈"]
+  },
+  {
+    id: "s003",
+    name: "成都",
+    artist: "赵雷",
+    mood: "民谣",
+    tags: ["民谣", "城市"]
+  },
+  {
+    id: "s004",
+    name: "后来",
+    artist: "刘若英",
+    mood: "回忆",
+    tags: ["经典", "情歌"]
+  },
+  {
+    id: "s005",
+    name: "夜空中最亮的星",
+    artist: "逃跑计划",
+    mood: "热烈",
+    tags: ["摇滚", "合唱"]
+  },
+  {
+    id: "s006",
+    name: "稻香",
+    artist: "周杰伦",
+    mood: "轻快",
+    tags: ["流行", "鼓励"]
+  },
+  {
+    id: "s007",
+    name: "平凡之路",
+    artist: "朴树",
+    mood: "远方",
+    tags: ["民谣", "公路"]
+  },
+  {
+    id: "s008",
+    name: "南山南",
+    artist: "马頔",
+    mood: "安静",
+    tags: ["民谣", "故事"]
+  },
+  {
+    id: "s009",
+    name: "小幸运",
+    artist: "田馥甄",
+    mood: "甜",
+    tags: ["流行", "电影"]
+  },
+  {
+    id: "s010",
+    name: "光辉岁月",
+    artist: "Beyond",
+    mood: "高昂",
+    tags: ["粤语", "经典", "合唱"]
+  }
+];
+
+module.exports = songs;
