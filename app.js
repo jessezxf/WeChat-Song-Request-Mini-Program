e11cf8aad7d13a162b18616c7d5dafb41bbcb10b
jesse <94545107+jessezxf@users.noreply.github.com>
2026-05-07 20:58:46 +0800

    jj
    add option

---
+++ app.js
@@ -0,0 +1,12 @@
+App({
+  globalData: {
+    performerName: "街角音乐人",
+    storageKeys: {
+      requests: "song_requests",
+      suggestions: "song_suggestions",
+      messages: "audience_messages",
+      suggestionDraft: "suggestion_draft",
+      audienceProfile: "audience_profile"
+    }
+  }
+});
